5e896b5cf932f66044cbb8144768c30fee02f5e9
Ture Farwell <farwelltj1988@gmail.com>
2013-02-21 16:51:46 -0500

    reivsed the app.py file to make route clearer to see and fixed issue with broken links involving assignment 1 and forum

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ app = Flask(__name__)
 def hello():
     return redirect(url_for('static', filename='Index.html'))
 
-# # Assignment 1: Chapter two section 2.4-2.8
-# @app.route('/assignment1')
-# def assignment_1():
+# # Assignment 1: Chapter two section 2.1-2.8
+# @app.route('/assignment')
+# def assignment1():
 #     return redirect(url_for('static', filename='assignment.html'))
 
-# # Assignment 1: Chapter two section 2.4-2.8
-# @app.route('/assignment1')
-# def assignment_1():
+# # Assignment 2: Chapter two section 2.9 
+# @app.route('/assignmentforum')
+# def assignmentforum():
 #     return redirect(url_for('static', filename='assignmentforum.html'))
 
 
 # # Assignment 3: Exercise 3.1
-# @app.route('/assignment-3')
-# def assignment_3():
+# @app.route('/assignment3')
+# def assignment3():
 #     return redirect(url_for('static', filename='assignment3.html'))
 
 
