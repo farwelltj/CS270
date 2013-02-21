7f419f3050d5eb27fd37bee49fd078f8fa71f0c4
Ture Farwell <farwelltj1988@gmail.com>
2013-02-21 16:19:07 -0500

    Added homework page route

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
+
+# Home page. Index link for all assignments
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return redirect(url_for('static', filename='index.html'))
+
+# # Assignment 1: Chapter two section 2.4-2.8
+# @app.route('/assignment1')
+# def assignment_1():
+#     return redirect(url_for('static', filename='assignment.html'))
+
+# # Assignment 1: Chapter two section 2.4-2.8
+# @app.route('/assignment1')
+# def assignment_1():
+#     return redirect(url_for('static', filename='assignmentforum.html'))
+
+
+# # Assignment 3: Exercise 3.1
+# @app.route('/assignment-3')
+# def assignment_3():
+#     return redirect(url_for('static', filename='assignment3.html'))
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
